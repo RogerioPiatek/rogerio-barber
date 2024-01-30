aad9697ff82a0f5014bfb44300a6c2cf0f4da4ec
Rogerio <rogeriopiatek@gmail.com>
2024-01-30 00:08:17 -0300

    feat: add initial barbershop item

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,8 +4,13 @@ import { ptBR } from "date-fns/locale";
 import Header from "../_components/header";
 import Search from "./_components/search";
 import BookingItem from "../_components/booking-item";
+import { db } from "../_lib/prisma";
+import BarbershopItem from "./_components/barbershop-item";
 
-export default function Home() {
+export default async function Home() {
+  //calling Prisma to take barbershops
+  const barbershops = await db.barbershop.findMany({});
+
   return (
     <div>
       <Header />
@@ -13,6 +18,7 @@ export default function Home() {
       <div className="px-5 pt-5">
         <h2 className="text-xl font-bold">Olá, Miguel!</h2>
         <p className="capitalize text-sm">
+          {/* using library date-fns to format dates */}
           {format(new Date(), "EEEE',' dd 'de' MMMM", {
             locale: ptBR,
           })}
@@ -28,6 +34,18 @@ export default function Home() {
           Agendamentos
         </h2>
         <BookingItem />
+      </div>
+
+      <div className="mt-6">
+        <h2 className=" px-5 text-xs mb-3 uppercase text-gray-400 font-bold">
+          Agendamentos
+        </h2>
+
+        <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
       </div>
     </div>
   );
