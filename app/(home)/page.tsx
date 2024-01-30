b64699583136869fa7c7c4b3d5fe59f03fcf909c
Rogerio <rogeriopiatek@gmail.com>
2024-01-29 22:22:43 -0300

    feat: add initial search component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,6 +2,7 @@ import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
 
 import Header from "../_components/header";
+import Search from "./_components/search";
 
 export default function Home() {
   return (
@@ -15,6 +16,10 @@ export default function Home() {
             locale: ptBR,
           })}
         </p>
+      </div>
+
+      <div className="px-5 mt-6">
+        <Search />
       </div>
     </div>
   );
