d424cf30163267c292671308e680b58e491d83e5
Rogerio <rogeriopiatek@gmail.com>
2024-01-29 22:11:30 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,21 @@
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
+
 import Header from "../_components/header";
 
 export default function Home() {
   return (
     <div>
       <Header />
+
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Miguel!</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR,
+          })}
+        </p>
+      </div>
     </div>
   );
 }
