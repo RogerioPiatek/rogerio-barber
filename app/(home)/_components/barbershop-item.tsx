f437db91a1c2eefd7fd392fcc5e1e36b6e2f3f53
Rogerio <rogeriopiatek@gmail.com>
2024-01-30 23:24:40 -0300

    feat: add ServiceItem component

--- app/(home)/_components/barbershop-item.tsx
+++ app/(home)/_components/barbershop-item.tsx
@@ -1,15 +1,25 @@
+"use client";
+
 import { Badge } from "@/app/_components/ui/badge";
 import { Button } from "@/app/_components/ui/button";
 import { Card, CardContent } from "@/app/_components/ui/card";
 import { Barbershop } from "@prisma/client";
 import { StarIcon } from "lucide-react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 interface BarbershopItemProps {
   barbershop: Barbershop;
 }
 
 const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
+  const router = useRouter();
+
+  // this function handles the click, redirecting to the proper barbershop by Id
+  const handleBookingClick = () => {
+    router.push(`/barbershops/${barbershop.id}`);
+  };
+
   return (
     <Card className="min-w-[167px] max-w-[167px] rounded-2xl">
       <CardContent className="px-1 py-0">
@@ -40,7 +50,11 @@ const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
           <p className="text-sm text-gray-400 overflow-hidden text-ellipsis text-nowrap">
             {barbershop.address}
           </p>
-          <Button className="w-full mt-3" variant="secondary">
+          <Button
+            onClick={handleBookingClick}
+            className="w-full mt-3"
+            variant="secondary"
+          >
             Reservar
           </Button>
         </div>
---
+++ app/barbershops/[id]/page.tsx
@@ -0,0 +1,51 @@
+import { Button } from "@/app/_components/ui/button";
+import { db } from "@/app/_lib/prisma";
+import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
+import Image from "next/image";
+import BarbershopInfo from "./_components/barbershop-info";
+import ServiceItem from "./_components/service-item";
+
+interface BarbershopDetailsPageProps {
+  params: {
+    id?: string;
+  };
+}
+
+const BarbershopDetailsPage = async ({
+  params,
+}: BarbershopDetailsPageProps) => {
+  //Ensure we redirect in case of no Id provided
+  if (!params.id) {
+    //TODO: redirect to home page
+    return null;
+  }
+
+  // Ensure we query based on the Id received on the URL
+  const barbershop = await db.barbershop.findUnique({
+    where: {
+      id: params.id,
+    },
+    include: {
+      services: true,
+    },
+  });
+
+  //TODO: redirect to home page
+  if (!barbershop) return null;
+
+  // Component for Info because it needs interactivity
+  return (
+    <div>
+      <BarbershopInfo barbershop={barbershop} />
+
+      {/* Render ServiceItem component based on the number of services (map), passing service as prop */}
+      <div className="px-5 flex flex-col gap-4 py-6">
+        {barbershop.services.map((service) => (
+          <ServiceItem key={service.id} service={service} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default BarbershopDetailsPage;
