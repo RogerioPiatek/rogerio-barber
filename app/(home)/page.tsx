b53e1d7d6f48e545cdfe0bbb7936c4203d6aa027
Rogerio <rogeriopiatek@gmail.com>
2024-02-02 17:08:43 -0300

    feat: add user name to home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -35,7 +35,11 @@ export default async function Home() {
       <Header />
 
       <div className="px-5 pt-5">
-        <h2 className="text-xl font-bold">Olá, Miguel!</h2>
+        <h2 className="text-xl font-bold">
+          {session?.user
+            ? `Olá, ${session.user.name?.split(" ")[0]}!`
+            : "Olá, vamos agendar um corte hoje?"}
+        </h2>
         <p className="capitalize text-sm">
           {/* using library date-fns to format dates */}
           {format(new Date(), "EEEE',' dd 'de' MMMM", {
